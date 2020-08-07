b2e70e41aef2247da21628f60d2d0cff3fbcd232
SShohag <shohag7m@gmail.com>
2020-08-08 01:39:22 +0600

    i do this

--- question.js
+++ question.js
@@ -1,8 +1,11 @@
-//Question one 
-if( 'false' ){
-    console.log("I am not false");
-} 
-else{console.log("I am false");
-}
-//Question two
-
+const students = [
+    {name:'santo', id:1510},
+    {name:'suvo', id:1511}
+];
+nameArray = [];
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    const nameList = element.name;
+    const ids = element.id;
+    console.log(nameList, ids);  
+};
